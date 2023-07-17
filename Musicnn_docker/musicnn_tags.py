91f6efe121fc07699dfd1c156a8d31a243599d71
DavidSolan0 <jdsolanov@unal.edu.co>
2023-07-17 23:51:47 +0200

    empty commit

--- Musicnn_docker/musicnn_tags.py
+++ Musicnn_docker/musicnn_tags.py
@@ -6,15 +6,15 @@ from musicnn.tagger import top_tags
 
 
 def extract(song):
-    tops = top_tags(song, model='MTT_musicnn', topN=10)
-    tags = ', '.join(tops)
+    tops = top_tags(song, model="MTT_musicnn", topN=10)
+    tags = ", ".join(tops)
     return tags
 
 
 if __name__ == "__main__":
-    song = '/data/song_infered.mp3'
+    song = "/data/song_infered.mp3"
     tops_string = extract(song)
-    print(tops_string)
     text_file = open("/data/tags_infered.txt", "w")
+    print(tops_string)
     text_file.write(tops_string)
     text_file.close()
